46448acb1a368883d38d39a7953d632ac05e3036
KiranaAdityaMoza <kiranamoza4@gmail.com>
2026-03-09 10:08:30 +0700

    Tugas LKP 3

--- app/src/main/java/com/example/praktam2_2417051057/MainActivity.kt
+++ app/src/main/java/com/example/praktam2_2417051057/MainActivity.kt
@@ -6,21 +6,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Card
-import androidx.compose.ui.layout.ContentScale
 import com.example.praktam2_2417051057.ui.theme.PrakTAM2_2417051057Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,50 +28,85 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PrakTAM2_2417051057Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DaftarBMIScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
+fun DaftarBMIScreen() {
 
-    LazyColumn(
-        modifier = modifier
+    Column(
+        modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
     ) {
 
-        items(BMIsource.dummyBMI) { bmi ->
+        Text(
+            text = "Kategori BMI",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold
+        )
 
-            Card(
+        Spacer(modifier = Modifier.height(16.dp))
+
+        BMIsource.dummyBMI.forEach { bmi ->
+
+            DetailBMIScreen(bmi)
+
+            Spacer(modifier = Modifier.height(24.dp))
+        }
+    }
+}
+
+@Composable
+fun DetailBMIScreen(bmi: Model.BMI) {
+
+    Card(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+
+            Image(
+                painter = painterResource(id = bmi.imageRes),
+                contentDescription = bmi.nama,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = bmi.nama,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(
+                text = bmi.deskripsi
+            )
+
+            Text(
+                text = "Nilai BMI: ${bmi.nilai}"
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Button(
+                onClick = { },
                 modifier = Modifier.fillMaxWidth()
             ) {
-
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-
-                    Text(text = "Kategori: ${bmi.nama}")
-                    Text(text = "Keterangan: ${bmi.deskripsi}")
-                    Text(text = "Nilai BMI: ${bmi.nilai}")
-
-                    Spacer(modifier = Modifier.height(8.dp))
-
-                    Image(
-                        painter = painterResource(id = bmi.imageRes),
-                        contentDescription = bmi.nama,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(180.dp)
-                    )
-                }
+                Text("Cek BMI")
             }
         }
     }
@@ -80,8 +114,8 @@ fun Greeting(modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DaftarBMIPreview() {
     PrakTAM2_2417051057Theme {
-        Greeting()
+        DaftarBMIScreen()
     }
 }
